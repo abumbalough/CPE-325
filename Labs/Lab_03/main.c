900e12035a21ffb7e372691e18f9b9adca3265a2
Austin T Bumbalough <atb0031@uah.edu>
2019-09-10 09:20:10 -0500

    Lab_03 solution in progress.

--- Labs/Lab_03/main.c
+++ Labs/Lab_03/main.c
@@ -1,22 +1,24 @@
 #include <msp430.h> 
 #include <stdio.h>
 
+#define SW1 P1IN & BIT0
+#define SW2 P1IN & BIT1
 
 /*
 --------------------------------------------------------
-* 
-File: 
-* 
-Description: 
-* 
+*
+File:
+*
+Description:
+*
 Input:
-* 
-Output: 
-* 
+*
+Output:
+*
 Author: Austin Bumbalough
-* 
+*
 Lab Section: 8
-* 
+*
 Date:
 *
 --------------------------------------------------------
@@ -24,7 +26,28 @@ Date:
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    // Initialize Pins
+    P2DIR |= BIT2 + BIT1;
+
+    // Turn LEDs on
+    P2OUT |= BIT2 + BIT1;
+
+    // Infinite loop
+    for(;;) {
+
+        // If Switch 1 is pressed
+        if ((SW1) == 0) {
+
+            // 20 ms debounce
+            int i;
+            for(i=0;i<20000;i++);
+
+            // Toggle LED using XOR
+            P2OUT ^= BIT2;
+        }
+    }
+
+    return 0;
 }
