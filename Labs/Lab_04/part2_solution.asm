9a33bbe862bc5a0b0a7736b9d8975aa704f68733
Austin T Bumbalough <atb0031@uah.edu>
2019-09-19 09:23:19 -0500

    Continued Lab 04 Part 2 implementation.

--- Labs/Lab_04/part2_solution.asm
+++ Labs/Lab_04/part2_solution.asm
@@ -49,37 +49,69 @@ RESET:       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 ;-------------------------------------------------------------------------------
 
 			clr.w SR
-			clr.w R5
-			bis.w #0xFF, P2DIR
-			
-			mov.w #evalString, R4
-			mov.b @R4+, R5 ; R5 stores expression result
-			cmp.b #0, R5
+			clr.w R7
+			bis.b #0xFF, P2DIR
+			mov.b #'+', R6
+
+getNext:	mov.b @R4+, R5 ; Move next operand to R7
+			cmp.b #0, R5 ; Check for string end
 			jeq printResult
-			mov.b @R4+, R6 ; R6 stores current operator
-			cmp.b #0, R6
-			jeq printResult
-			mov.b @R4+, R7 ; R7 stores operand
-			cmp.b #0, R7
-			jeq printResult
+			jmp charToDec
+charToDec:	cmp.b #'0', R5
+			jeq ascii0
+			cmp.b #'1', R5
+			jeq ascii1
+			cmp.b #'2', R5
+			jeq ascii2
+			cmp.b #'3', R5
+			jeq ascii3
+			cmp.b #'4', R5
+			jeq ascii4
+			cmp.b #'5', R5
+			jeq ascii5
+			cmp.b #'6', R5
+			jeq ascii6
+			cmp.b #'7', R5
+			jeq ascii7
+			cmp.b #'8', R5
+			jeq ascii8
+			cmp.b #'9', R5
+			jeq ascii9
+ascii0:		mov.b #0, R5
+			jmp getOp
+ascii1:		mov.b #1, R5
+			jmp getOp
+ascii2:		mov.b #2, R5
+			jmp getOp
+ascii3:		mov.b #3, R5
+			jmp getOp
+ascii4:		mov.b #4, R5
+			jmp getOp
+ascii5:		mov.b #5, R5
+			jmp getOp
+ascii6:		mov.b #6, R5
+			jmp getOp
+ascii7:		mov.b #7, R5
+			jmp getOp
+ascii8:		mov.b #8, R5
+			jmp getOp
+ascii9:		mov.b #9, R5
+			jmp getOp
+eval:		cmp.b #0, R8
+			jeq setAdd
+			cmp.b #1, R8
+			jeq setSub
+getOp:		mov.b @R4+, R6 ; Get next Operator
 			cmp.b #'+', R6 ; Check if operator is +
 			jeq addOps
 			cmp.b #'-', R6 ; Check if operator is -
 			jeq subOps
-getNext:	mov.b @R4+, R7 ; Move next operand to R7
-			cmp.b #0, R7 ; Check for string end
-			jeq printResult
-			mov.b @R4+, R6 ; Get next Operator
-			cmp.b #0, R6 ; Check for string end
-			jeq printResult
-			cmp.b #'+', R6 ; Check if operator is +
-			jeq addOps
-			cmp.b #'-', R6 ; Check if operator is -
-			jeq subOps
-addOps:		add.w R7, R5 ; Add R7 to R5
+addOps:		add.b R5, R7 ; Add R7 to R5
 			jmp getNext
-subOps:		sub.w R7, R5 ; Subtract R7 from R5
+subOps:		sub.b R5, R7 ; Subtract R7 from R5
 			jmp getNext
+setAdd:		mov.b #0, R8
+setSub:		mov.b #1, R8
 printResult:mov.b R5, P2OUT
 			jmp $
 
