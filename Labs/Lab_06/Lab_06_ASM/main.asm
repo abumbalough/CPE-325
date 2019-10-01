0acca3cfad83ec17182c62df8bc609cdc9340311
Austin T Bumbalough <atb0031@uah.edu>
2019-10-01 09:20:03 -0500

    Lab_06 Initial commit.

--- Labs/Lab_06/Lab_06_ASM/main.asm
+++ Labs/Lab_06/Lab_06_ASM/main.asm
@@ -1,6 +1,26 @@
 ;-------------------------------------------------------------------------------
 ; MSP430 Assembler Code Template for use with TI Code Composer Studio
 ;
+; *
+; File: Lab_06/Lab_06_ASM/main.asm
+; *
+; Description: Use interrupt service routines to blink LEDs according to pressed switches
+; *
+; Input: On-board switches SW1 and SW2
+; *
+; Output: On-board LEDs LED1 and LED2
+; *
+; Author: Austin Bumbalough
+; *
+; Lab Section: 8
+; *
+; Date: 10/08/2019
+; *
+;
+; SW1 - P1.0
+; SW2 - P1.1
+; LED1 - P2.2
+; LED2 - P2.1
 ;
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
@@ -16,8 +36,15 @@
                                             ; references to current section.
 
 ;-------------------------------------------------------------------------------
-RESET:       mov.w   #__STACK_END,SP         ; Initialize stackpointer
-StopWDT:     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+RESET:		mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT:	mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+Setup:		bic.w BIT1|BIT0, P1DIR 		; Configure P1.0 and P1.1 as inputs
+			bis.w BIT2|BIT1, P2DIR 		; Configure P2.1 and P2.2 as ouputs
+			bis.w #GIE, SR 				; Enable global interrupts
+			bis.w BIT1|BIT0, P1IES 		; Trigger interrupts on falling edge since SW1 and SW2 have 100K pullup resistor
+			bic.w BIT1|BIT0, P1IFG 		; Clear any pending interrupts
+			bis.w BIT1|BIT0, P1IE 		; Enable interrupts on P1.0 and P1.1
 
 
 ;-------------------------------------------------------------------------------
---
+++ Labs/Lab_06/README.md
@@ -0,0 +1,6 @@
+Austin Bumbalough  
+CPE 325-08  
+Lab 06  
+10/08/2019  
+# Lab 6 Solution  
+Lab 6 solution is not yet implemented.
